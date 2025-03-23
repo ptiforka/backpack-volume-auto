19266399f3c5fea2f706e5b9e1c2818aa38d93a9
ptiforka <35385354+ptiforka@users.noreply.github.com>
2025-03-23 18:40:28 +0100

    Update config.py

--- inputs/config.py
+++ inputs/config.py
@@ -4,7 +4,7 @@ THREADS = 1  # Enter amount of threads
 DELAY_BETWEEN_TRADE = (1, 2)  # Execute delay between every trade (Buy -> Delay -> Sell -> Buy -> Delay ...)
 DELAY_BETWEEN_DEAL = (1, 2)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
 
-NEEDED_TRADE_VOLUME = 100_000  # volume to trade, if 0 it will never stop
+NEEDED_TRADE_VOLUME = 0  # volume to trade, if 0 it will never stop
 MIN_BALANCE_TO_LEFT = 0# min amount to left on the balance, if 0, it is traded until the balance is equal to 0.
 
 TRADE_AMOUNT = [0, 0]  # (works nearly from +-20%), minimum (5$) and maximum amount to trade in USD, if 0 it will trade on FULL balance
