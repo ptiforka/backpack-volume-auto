2105515733fb64b92a09aeaececf5f6e6f1cee90
ptiforka <35385354+ptiforka@users.noreply.github.com>
2025-03-25 13:44:36 +0100

    Update config.py

--- inputs/config.py
+++ inputs/config.py
@@ -8,7 +8,7 @@ NEEDED_TRADE_VOLUME = 0  # volume to trade, if 0 it will never stop
 MIN_BALANCE_TO_LEFT = 0# min amount to left on the balance, if 0, it is traded until the balance is equal to 0.
 
 TRADE_AMOUNT = [0, 0]  # (works nearly from +-20%), minimum (5$) and maximum amount to trade in USD, if 0 it will trade on FULL balance
-ALLOWED_ASSETS = ["SOL_USDC", "JUP_USDC", "BTC_USDC"]
+ALLOWED_ASSETS = ["SOL_USDC", "USDT_USDC"]
 
 # Volatility moment
 # DEPTH of limit order to trade as market order
